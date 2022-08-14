b78e30d4bb78623444d25fa467d6339fbf6a6673
ayushjain27 <ayushjain201574@gmail.com>
2022-08-14 09:45:10 +0530

    Updated

--- src/Components/Videos/Recording.jsx
+++ src/Components/Videos/Recording.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from './Recording.module.css';
 import { useNavigate } from "react-router-dom";
-
+// import { ReactMediaRecorder } from "react-media-recorder";
 
 const Recording = () => {
     const navigate = useNavigate();
     const handleRecording = () => {
         navigate('/introduce')
     }
+
   return (
     <>
         <div className={`${styles.container} container mt-5 mb-5`}>
         <div className="row h-100">
-          <div className="col-md-4" style={{backgroundColor: 'red'}}></div>
+          <div className="col-md-4" style={{backgroundColor: 'red'}}>        
+        </div>
           <div className="col-md-8 p-5 h-100">
           <h3 className='text-center mb-3'>STEP 2: Let's check your device</h3>
             <div className="fs-5 mb-2 ms-3 me-3">On your device, please grant access to your camera and microphone. Run a test recording to ensure you can see and hear yourself clearly.</div>
@@ -21,7 +23,8 @@ const Recording = () => {
           </div>
         </div>
       </div>
- 
+      <div>
+      </div>
     </>
   )
 }
